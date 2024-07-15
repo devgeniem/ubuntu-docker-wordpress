c54b2c7399ecfe2e6c5080fbfb4b9c971bafcff1
Jussi Alanen <jussi.alanen@hiondigital.com>
2024-07-15 12:39:24 +0300

    Add execute permission for run files on services

--- ubuntu-8.1/entrypoint.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-/etc/init.d/php8.1-fpm start
-/etc/init.d/php8.1-fpm status
-whoami
-# php-fpm -R -c /etc/php/8.1/fpm/php.ini -y /etc/php/8.1/fpm/fpm-pool.conf
-nginx -c /etc/nginx/nginx.conf -g 'daemon off;'
